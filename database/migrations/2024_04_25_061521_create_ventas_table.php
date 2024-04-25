8c213c3c32a4bba53a473b2fa0a929831edc6e3d
santiago goez <128654665+Santy0240@users.noreply.github.com>
2024-04-25 01:23:45 -0500

    migraciones

---
+++ database/migrations/2024_04_25_061521_create_ventas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ventas', function (Blueprint $table) {
+            $table->id('id_venta');
+            $table->integer('id_vehiculo');
+            $table->integer('id_cliente');
+            $table->integer('fecha_venta');
+            $table->integer('precio_final');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ventas');
+    }
+};
+
